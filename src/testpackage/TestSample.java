1f264a0f63cc39168d24fd6dfb5303b70c60f674
sethujack <sethupathi004@gmail.com>
2019-05-07 13:26:15 +0530

    average of a and b

---
+++ src/testpackage/TestSample.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class TestSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10; int b=49;
+System.out.println("average of a and b:"+(a+b)/2);
+	}
+
+}
